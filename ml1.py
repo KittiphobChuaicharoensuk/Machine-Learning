7ed7fe173418fd90dfbe31c555901f801b49bc0e
KittiphobChuaicharoensuk <kittiphob.chuaicharoensuk@gmail.com>
2021-06-18 12:49:04 +0700

    ค่า Precicion Recall F1-Score

--- ml1.py
+++ ml1.py
@@ -1,8 +1,30 @@
 from scipy.io import loadmat
 from sklearn.linear_model import SGDClassifier
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_predict
+from sklearn.metrics import confusion_matrix,classification_report
+import itertools 
 import numpy as np
 import matplotlib.pyplot as plt
+
+def displayConfusionMatrix(cm,cmap=plt.cm.Spectral):
+    classes_x=["Other Number","Number 5"]
+    classes_y=["Other Number Group","Number 5 Group"]
+    plt.imshow(cm,interpolation='nearest',cmap=cmap)
+    plt.title("Confusion Matrix")
+    plt.colorbar()
+    trick_marks=np.arange(len(classes_x))
+    plt.xticks(trick_marks,classes_x)
+    plt.yticks(trick_marks,classes_y)
+    thresh=cm.max()/2
+    for i , j in itertools.product(range(cm.shape[0]),range(cm.shape[1])):
+        plt.text(j,i,format(cm[i,j],'d'),
+        horizontalalignment='center',
+        color='white' if cm[i,j]>thresh else 'black')
+
+    plt.tight_layout()
+    plt.ylabel('Actually')
+    plt.xlabel('Prediction')
+    plt.show()
 
 def displayImage(x):
     plt.imshow(
@@ -13,7 +35,7 @@ def displayImage(x):
 
 def displayPredict(clf, actually_y,x_test):
     print("Actually:",actually_y)
-    print("Prediction:",clf.predict([x_test][0]))   
+    print("Prediction:",clf.predict([x_test])[0])   
  
 mnist_raw = loadmat("mnist-original.mat")
 mnist = {
@@ -27,16 +49,20 @@ xtrain,xtest,ytrain,ytest = x[:60000], x[60000:], y[:60000],y[60000:]
 
 #class 0, class not 0
 #ข้อมูล 1 ค่า -> data model ->class 0 ? true: false 
-predict_number = 5000
+predict_number = 6000
 #create boolean array if the value equals zero
 ytrain_5 = (ytrain==5)
 ytest_5 = (ytest==5)
-
 
 sgd_clf = SGDClassifier() 
 """สร้าง Object SGD"""
 sgd_clf.fit(xtrain,ytrain_5)
 """เทรนข้อมูลทั้งหมดโดยเช็ค class""" 
 
-score = cross_val_score(sgd_clf,xtrain,ytrain_5,cv=3,scoring="accuracy")
-print(score)
+ytrain_pred = cross_val_predict(sgd_clf,xtrain,ytrain_5,cv=3)
+cm=confusion_matrix(ytrain_5,ytrain_pred)
+
+ytest_pred = sgd_clf.predict(xtest)
+
+classes = ["Other numbers","Number 5"]
+print(classification_report(ytest_pred,ytest_5,target_names=classes))
